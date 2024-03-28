cb7fe1bce213aac6911018f2d5bf255f16ca8c10
ntEOS <nguyentoan231196@gmail.com>
2024-03-28 21:51:04 +0700

    implement middleware to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { LanguagesModule } from "./languages/languages.module";
+import { LoggerMiddleware } from "./common/middleware/logger.middleware";
+import { LanguagesController } from "./languages/languages.controller";
 
 @Module({
 	imports: [LanguagesModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		// consumer.apply(LoggerMiddleware).forRoutes("languages");
+
+		// consumer
+		// 	.apply(LoggerMiddleware)
+		// 	.forRoutes({ path: "languages", method: RequestMethod.GET });
+
+		consumer.apply(LoggerMiddleware).forRoutes(LanguagesController);
+	}
+}
